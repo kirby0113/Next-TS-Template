3fa6e6fd2c9c653edf7682b3e6209e512a9e0401
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-02 13:48:17 +0900

    fix: jestの設定ファイル修正

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   setupFilesAfterEnv: ["./jest.setup.js"],
   setupFiles: ["dotenv/config"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      tsconfig: "./tsconfig.test.json",
+    },
+  },
 };
