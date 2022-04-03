248c9833fe30c40c5d936d0eb2b8d7a2f29bab35
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-03 14:54:48 +0900

    fix: jestのテスト用にコード修正

--- jest.setup.js
+++ jest.setup.js
@@ -1,24 +1,24 @@
-// Polyfill "window.fetch" used in the React component.
-import "whatwg-fetch";
+// // Polyfill "window.fetch" used in the React component.
+// import "whatwg-fetch";
 
-import { server } from "./mocks/server";
-import { handlers, init } from "./mocks/handlers/handlers";
-import { cleanup } from "@testing-library/react";
+// import { server } from "./mocks/server";
+// import { handlers, init } from "./mocks/handlers/handlers";
+// import { cleanup } from "@testing-library/react";
 
-beforeAll(() => {
-  server.listen();
-});
+// beforeAll(() => {
+//   server.listen();
+// });
 
-afterEach(() => {
-  cleanup();
-  server.resetHandlers();
-  init();
-});
+// afterEach(() => {
+//   cleanup();
+//   server.resetHandlers();
+//   init();
+// });
 
-afterAll(() => {
-  server.close();
-});
+// afterAll(() => {
+//   server.close();
+// });
 
-HTMLCanvasElement.prototype.getContext = () => {
-  // return whatever getContext has to return
-};
+// HTMLCanvasElement.prototype.getContext = () => {
+//   // return whatever getContext has to return
+// };
